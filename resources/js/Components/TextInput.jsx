6a7b0ce94b6ad0ea4c957e5eaeba28c29b52ea53
Grandel AGBANOU <grandelagbanou28@gmail.com>
2026-06-14 00:00:00 +0200

    style: enhance TextInput with icon support

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,6 +1,6 @@
 import { forwardRef, useEffect, useRef } from 'react';
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, icon: Icon, ...props }, ref) {
     const input = ref ? ref : useRef();
 
     useEffect(() => {
@@ -10,14 +10,18 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
     }, []);
 
     return (
-        <input
-            {...props}
-            type={type}
-            className={
-                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
-                className
-            }
-            ref={input}
-        />
+        <div className="relative">
+            {Icon && (
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                    <Icon className="h-5 w-5 text-gray-400 dark:text-gray-500" />
+                </div>
+            )}
+            <input
+                {...props}
+                type={type}
+                className={`input-field ${Icon ? 'pl-10' : 'pl-3'} ${className}`}
+                ref={input}
+            />
+        </div>
     );
 });
