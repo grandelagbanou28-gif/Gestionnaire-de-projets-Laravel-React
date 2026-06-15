3f59b36d84db7d2802ba6d024e173099de4b02b3
Grandel AGBANOU <grandelagbanou28@gmail.com>
2026-06-16 00:00:00 +0200

    feat: auto-verify email on registration

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,9 +40,11 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'locale' => 'en',
+            'email_verified_at' => now(),
         ]);
 
-        event(new Registered($user));
+        // event(new Registered($user));
 
         Auth::login($user);
 
